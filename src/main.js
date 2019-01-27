ac9f8ca6fdfc40cb9fe73fe2e4c498b3efe3fe40
Barry Li <bli031@uottawa.ca>
2019-01-27 00:17:55 -0500

    simple navbar

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ class NavBar extends React.Component {
 		let className = 'nav-bar';
 		return (
 			<nav className={className}>
-				NavBar
+				<li><a href='#'>Home</a></li>
+				<li><a href='#'>Doc</a></li>
+				<li><a href='#'>Source</a></li>
+				<li><a href='#'>Contact</a></li>
 			</nav>
 		);
 	}
