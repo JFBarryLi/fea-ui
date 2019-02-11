35bc04314e44be83110bbe6383121d1b51754d6e
Barry Li <bli031@uottawa.ca>
2019-02-10 23:58:32 -0500

    added threejs renderer

--- src/structure.js
+++ src/structure.js
@@ -12,27 +12,12 @@ class Options extends React.Component {
 	}
 }
 
-class Plotter extends React.Component {
-	render() {
-		let className = 'plotter';
-		return (
-			<canvas className={className}>
-			</canvas>
-		);
-	}
-}
-
 class PlotterContainer extends React.Component {
 	render() {
 		let className = 'plotter-container';
 		return (
 		<div className={className}>
-			<Options
-
-			/>
-			<Plotter
-			
-			/>
+			<Options />
 		</div>
 		);
 	}
@@ -334,3 +319,143 @@ ReactDOM.render(
 	<Layout />,
 	document.getElementById('root')
 );
+
+function plotter() {
+
+	// Initialize variables
+	var container;
+	var camera, scene, renderer;
+	var linePointsLength;
+	this.lineHelperObjects = [];
+	var positions = [];
+	var point = new THREE.Vector3();
+	var geometry = new THREE.SphereBufferGeometry( 20, 20, 20 );
+	
+	init();
+	
+	function init() {
+		var container = document.getElementsByClassName("plotter-container")[0];
+		var options = document.getElementsByClassName("options")[0];
+		
+		// Scene
+		scene = new THREE.Scene();
+		scene.background = new THREE.Color( 0xf0f0f0 );
+		camera = new THREE.PerspectiveCamera( 70, window.innerWidth / (window.innerHeight-300), 1, 10000 );
+		camera.position.set( 0, 250, 1000 );
+		scene.add( camera );
+		
+		// Lighting
+		scene.add( new THREE.AmbientLight( 0xf0f0f0 ) );
+		var light = new THREE.SpotLight( 0xffffff, 1.5 );
+		light.position.set( 0, 1500, 200 );
+		light.castShadow = true;
+		light.shadow = new THREE.LightShadow( new THREE.PerspectiveCamera( 70, 1, 200, 2000 ) );
+		light.shadow.bias = - 0.000222;
+		light.shadow.mapSize.width = 1024;
+		light.shadow.mapSize.height = 1024;
+		scene.add( light );
+		
+		// Plane
+		var planeGeometry = new THREE.PlaneBufferGeometry( 2000, 2000 );
+		planeGeometry.rotateX( - Math.PI / 2 );
+		var planeMaterial = new THREE.ShadowMaterial( { opacity: 0.2 } );
+		var plane = new THREE.Mesh( planeGeometry, planeMaterial );
+		plane.position.y = - 200;
+		plane.receiveShadow = true;
+		scene.add( plane );
+		
+		// Helper
+		var helper = new THREE.GridHelper( 2000, 100 );
+		helper.position.y = - 199;
+		helper.material.opacity = 0.25;
+		helper.material.transparent = true;
+		scene.add( helper );
+
+		renderer = new THREE.WebGLRenderer( { antialias: true } );
+		renderer.setPixelRatio( window.devicePixelRatio );
+		renderer.setSize( window.innerWidth, (window.innerHeight-300) );
+		renderer.shadowMap.enabled = true;
+		container.insertBefore(renderer.domElement, options);
+
+		// Controls
+		var controls = new THREE.OrbitControls( camera, renderer.domElement );
+		controls.damping = 0.2;
+		controls.addEventListener( 'change', render );
+			
+	}
+	
+	// Add a node to plot
+	this.addNodeObject = function( position ) {
+		var material = new THREE.MeshLambertMaterial( { color: 0x705D56 } );
+		var object = new THREE.Mesh( geometry, material );
+		if ( position ) {
+			object.position.copy( position );
+		}
+		object.castShadow = true;
+		object.receiveShadow = true;
+		scene.add( object );
+		this.lineHelperObjects.push( object );
+		render();
+		return object;
+	}
+	function addPoint() {
+		linePointsLength ++;
+		positions.push( this.addNodeObject().position );
+	}
+	function removePoint() {
+		linePointsLength --;
+		positions.pop();
+		scene.remove( this.lineHelperObjects.pop() );
+	}
+
+	// Load a set of nodes
+	this.load = function( new_positions ) {
+		linePointsLength = new_positions.length;
+		for ( var i = 0; i < linePointsLength; i ++ ) {
+			this.addNodeObject( positions[ i ] );
+		}
+		positions = [];
+		for ( var i = 0; i < linePointsLength; i ++ ) {
+			positions.push( this.lineHelperObjects[ i ].position );
+		}
+		
+		while ( new_positions.length > positions.length ) {
+			addPoint();
+		}
+		while ( new_positions.length < positions.length ) {
+			removePoint();
+		}
+		for ( var i = 0; i < positions.length; i ++ ) {
+			positions[ i ].copy( new_positions[ i ] );
+		}
+		render();
+	}
+
+	function render() {
+		renderer.render( scene, camera );
+	}
+	
+	// Dynamic Resizing
+	window.addEventListener( 'resize', onWindowResize, false );
+	
+	function onWindowResize() {
+		if (window.innerWidth <= 1200) {
+			camera.aspect = window.innerWidth / (window.innerHeight-300);
+			camera.updateProjectionMatrix();
+			renderer.setSize( window.innerWidth, (window.innerHeight-300) );
+			render();
+		} else {
+			camera.aspect = 1200 / (window.innerHeight-300);
+			camera.updateProjectionMatrix();
+			renderer.setSize( 1200, (window.innerHeight-300) );
+			render();
+		}
+	}
+}
+
+var plotter = new plotter();
+plotter.load( [ 
+	new THREE.Vector3( -100, 0, 0 ),
+	new THREE.Vector3( -100, 100, 0 ),
+	new THREE.Vector3( 100, 100, 0 ),
+	new THREE.Vector3( 100, 0, 0 ) ] );
